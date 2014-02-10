91c437aa394a483338f927627e92f8deabd8a1f1
Jens Krause <jens@propertyshelf.com>
2014-02-10 17:26:36 +0700

    add demo fields from collective.examples.userdata, fix __init__.py

--- src/customer/krainrealestate/__init__.py
+++ src/customer/krainrealestate/__init__.py
@@ -1,5 +1,2 @@
-# -*- extra stuff goes here -*-
-
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
+from zope.i18nmessageid import MessageFactory
+_ = MessageFactory("customer.krainrealestate")
