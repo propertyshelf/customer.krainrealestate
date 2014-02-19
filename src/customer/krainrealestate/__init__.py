6e507ddf87d3328e448763dc3e87c36fca11f2ca
Jens Krause <jens@propertyshelf.com>
2014-02-19 21:51:29 +0700

    try to mokeypatch Size of the member portrait

--- src/customer/krainrealestate/__init__.py
+++ src/customer/krainrealestate/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 from zope.i18nmessageid import MessageFactory
 _ = MessageFactory("customer.krainrealestate")
+
+
+def initialize(context):
+    from Products.PlonePAS import config
+    config.MEMBER_IMAGE_SCALE = (250, 500)
