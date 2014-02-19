54028903fe968be47670d94976a35c8c9595ae36
Jens Krause <jens@propertyshelf.com>
2014-02-19 21:50:15 +0700

    update fom_fields omits

--- src/customer/krainrealestate/browser/CustomizedUserDataPanel.py
+++ src/customer/krainrealestate/browser/CustomizedUserDataPanel.py
@@ -1,7 +1,7 @@
 from plone.app.users.browser.personalpreferences import UserDataPanel
 
 class CustomizedUserDataPanel(UserDataPanel):
+    """ Hide certain form fields in the UserDataPanel """
     def __init__(self, context, request):
         super(CustomizedUserDataPanel, self).__init__(context, request)
-        self.form_fields = self.form_fields.omit('location')
-        self.form_fields = self.form_fields.omit('description')
+        self.form_fields = self.form_fields.omit('location', 'description')
