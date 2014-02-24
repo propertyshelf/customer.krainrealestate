e85ea438e4e993908751779cd390f9df072a6242
Jens Krause <jens@propertyshelf.com>
2014-02-24 18:04:36 +0700

    add tests for new AgentSearchViewlet (='Choose your Agent')

--- src/customer/krainrealestate/tests/test_viewlets.py
+++ src/customer/krainrealestate/tests/test_viewlets.py
@@ -7,6 +7,8 @@ from zope.interface import alsoProvides
 from plone.app.layout.viewlets.tests.base import ViewletsTestCase
 from customer.krainrealestate.browser.agentprofile_viewlet import (AgentProfileViewlet, \
     IAgentProfile, IPossibleAgentProfile)
+from customer.krainrealestate.browser.agentsearch_viewlet import (AgentSearchViewlet, \
+    IAgentSearch, IPossibleAgentSearch)
 
 class TestKrainViewlet(ViewletsTestCase):
     """Test the custom viewlets"""
@@ -18,6 +20,8 @@ class TestKrainViewlet(ViewletsTestCase):
         self.folder.setTitle(u"Folder")
         alsoProvides(self.folder.test, IAgentProfile)
         alsoProvides(self.folder.test, IPossibleAgentProfile)
+        alsoProvides(self.folder.test, IAgentSearch)
+        alsoProvides(self.folder.test, IPossibleAgentSearch)
         self.folder.test.reindexObject(idxs=['object_provides', ])
         
 
@@ -38,5 +42,19 @@ class TestKrainViewlet(ViewletsTestCase):
         self.assertEqual(viewlet.site_url, "http://nohost/plone")
         self.assertFalse(viewlet.available)
         viewlet = AgentProfileViewlet(self.folder.test, self.app.REQUEST, None)
+        viewlet.update()
+        self.assertTrue(viewlet.available)
+
+    def test_AgentSearch_available(self):
+        """ Test for availabbility of AgentProfile Viewlet"""
+        self._invalidateRequestMemoizations()
+        self.loginAsPortalOwner()
+        self.app.REQUEST['ACTUAL_URL'] = self.folder.test.absolute_url()
+        
+        viewlet = AgentSearchViewlet(self.folder, self.app.REQUEST, None)
+        viewlet.update()
+        self.assertEqual(viewlet.site_url, "http://nohost/plone")
+        self.assertFalse(viewlet.available)
+        viewlet = AgentSearchViewlet(self.folder.test, self.app.REQUEST, None)
         viewlet.update()
         self.assertTrue(viewlet.available)
