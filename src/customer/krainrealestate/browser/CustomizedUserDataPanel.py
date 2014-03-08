662e2183815cc5085bcc906387d5ea3ef5d13995
Jens Krause <jens@propertyshelf.com>
2014-03-08 10:18:18 +0700

    hide the profile pages fields in Memberdata edit view

--- src/customer/krainrealestate/browser/CustomizedUserDataPanel.py
+++ src/customer/krainrealestate/browser/CustomizedUserDataPanel.py
@@ -4,4 +4,4 @@ class CustomizedUserDataPanel(UserDataPanel):
     """ Hide certain form fields in the UserDataPanel """
     def __init__(self, context, request):
         super(CustomizedUserDataPanel, self).__init__(context, request)
-        self.form_fields = self.form_fields.omit('location', 'description')
+        self.form_fields = self.form_fields.omit('location', 'description','agent_profile_en', 'agent_profile_es', 'agent_profile_de')
