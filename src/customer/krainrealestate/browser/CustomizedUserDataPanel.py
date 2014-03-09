976aef6b25c417c0986dd9e4d7901c9ba61fec19
Jens Krause <jens@propertyshelf.com>
2014-03-09 12:50:50 +0700

    implementing plone.app.users.browser.interfaces._on_save()

--- src/customer/krainrealestate/browser/CustomizedUserDataPanel.py
+++ src/customer/krainrealestate/browser/CustomizedUserDataPanel.py
@@ -5,3 +5,11 @@ class CustomizedUserDataPanel(UserDataPanel):
     def __init__(self, context, request):
         super(CustomizedUserDataPanel, self).__init__(context, request)
         self.form_fields = self.form_fields.omit('location', 'description','agent_profile_en', 'agent_profile_es', 'agent_profile_de')
+
+    def _on_save(self, data):
+        """ implementing plone.app.users.browser.interfaces._on_save function
+            for custom event handling
+        """
+        from pprint import pprint
+        pprint('User Data saved, hook is hooking!')
+        pprint(data)
